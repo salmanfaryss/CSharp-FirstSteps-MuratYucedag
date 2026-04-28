7c51a9883a1d0e4eab59baaafbd31d2e819fb2f7
Souleymane Tiendrebeogo <salmanfaryss@gmail.com>
2026-04-29 00:49:59 +0300

    CsharpFirstSteps

---
+++ CsharpEgitim_Kampi/01_MainSubjects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region yazdırma Komutları
+            //Console.WriteLine("Merhaba Dünya");
+            //Console.WriteLine("Selam");
+
+            Console.WriteLine("***** Yemek Kategoriler *****");
+
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
